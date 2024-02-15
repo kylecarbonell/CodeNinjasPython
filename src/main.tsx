e10fdbee765b4e4c39d1c35974efe803ca15f193
Kyle Carbonell <kylecarbonell13@gmail.com>
2024-02-14 18:27:07 -0800

    Working on dropdown area

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import ReactDOM from 'react-dom/client'
 // import App from './App.tsx'
-import Home from './components/Home.tsx'
+// import Home from './components/Home.tsx'
+import App from './App.tsx'
 // import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Home />
+  <App />
 )
