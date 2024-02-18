21580f0be64fc4b4a1c3ba310b7aa09123ea779a
Kyle Carbonell <kylecarbonell13@gmail.com>
2024-02-18 13:59:52 -0800

    Working on serverside

--- server/server.js
+++ server/server.js
@@ -19,57 +19,57 @@ puppeteer.use(StealthPlugin);
 dotenv.config();
 const PORT = 8000;
 
-app.get("/Create", async (req, res) => {
-  const myName = "KyleCarbonell";
+// app.get("/Create", async (req, res) => {
+//   const myName = "KyleCarbonell";
 
-  console.log("HERE FIRST");
-  let browser = await puppeteer.launch({
-    headless: false,
-    args: ["--disable-features=site-per-process"],
-  });
-  const page = await browser.newPage();
+//   console.log("HERE FIRST");
+//   let browser = await puppeteer.launch({
+//     headless: false,
+//     args: ["--disable-features=site-per-process"],
+//   });
+//   const page = await browser.newPage();
 
-  await page.goto("https://replit.com/~");
+//   await page.goto("https://replit.com/~");
 
-  //Enters Password
-  await page.waitForSelector('input[type="password"]');
-  await page.click('input[type="password"]');
-  await page.keyboard.type("Lu2nglu2");
+//   //Enters Password
+//   await page.waitForSelector('input[type="password"]');
+//   await page.click('input[type="password"]');
+//   await page.keyboard.type("Lu2nglu2");
 
-  //Enters Username
-  await page.type(".css-1ecwrb4", "razorpooandpee@gmail.com").then(async () => {
-    console.log("here");
-  });
+//   //Enters Username
+//   await page.type(".css-1ecwrb4", "razorpooandpee@gmail.com").then(async () => {
+//     console.log("here");
+//   });
 
-  //Clicks Login Button
+//   //Clicks Login Button
 
-  await page.keyboard.press("Enter").then(async () => {
-    await page.waitForSelector(".css-1dtnej2");
-    await page.goto("https://replit.com/@razorpooandpee");
+//   await page.keyboard.press("Enter").then(async () => {
+//     await page.waitForSelector(".css-1dtnej2");
+//     await page.goto("https://replit.com/@razorpooandpee");
 
-    //Clicks Create
-    // await page.click(".css-1dtnej2");
+//     //Clicks Create
+//     // await page.click(".css-1dtnej2");
 
-    //Choose python
-    await page.waitForSelector(".css-12qfl1s");
-    await page.keyboard.type("python");
-    await page.click(".css-155h4ok");
+//     //Choose python
+//     await page.waitForSelector(".css-12qfl1s");
+//     await page.keyboard.type("python");
+//     await page.click(".css-155h4ok");
 
-    //Names Activity
-    await page.waitForSelector(".css-wwxvxh").then(async () => {
-      await page.keyboard.type("KyleCarbonell-Activity1");
-      await page.keyboard.press("Enter").then(async () => {
-        await page.waitForSelector(".css-1loehem");
-        await page.click(".css-1loehem");
+//     //Names Activity
+//     await page.waitForSelector(".css-wwxvxh").then(async () => {
+//       await page.keyboard.type("KyleCarbonell-Activity1");
+//       await page.keyboard.press("Enter").then(async () => {
+//         await page.waitForSelector(".css-1loehem");
+//         await page.click(".css-1loehem");
 
-        await page.waitForSelector(".css-1si1nqs");
-        await page.click(".css-1si1nqs");
-      });
-    });
-  });
+//         await page.waitForSelector(".css-1si1nqs");
+//         await page.click(".css-1si1nqs");
+//       });
+//     });
+//   });
 
-  //Creates replit
-});
+//   //Creates replit
+// });
 
 app.get("/submit", async (req, res) => {
   res.send("submit");
