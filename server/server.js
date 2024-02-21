2d6eccc106e7a0cd93488378433cdfcc23c216ea
Kyle Carbonell <kylecarbonell13@gmail.com>
2024-02-21 13:01:09 -0800

    Transitioning to server

--- server/server.js
+++ server/server.js
@@ -92,6 +92,75 @@ app.get("/instructions", async (req, res) => {
   res.status(200);
 });
 
+app.post("/login", async (req, res) => {
+  console.log(req.body.username);
+  const user = { username: req.body.username };
+  const command = { $set: { signedIn: req.body.signIn } };
+  const login = await db.collection("Python").updateOne(user, command);
+  console.log(login);
+  if (
+    login.modifiedCount == 1 ||
+    (login.modifiedCount == 0 && login.matchedCount == 1)
+  ) {
+    console.log("good");
+    res.status(200);
+  } else {
+    console.log("bad");
+    res.status(202);
+  }
+  res.send();
+});
+
+/**
+ * Creates users account collection that
+ * stores their activities
+ *
+ * Headers : {username : name of the user}
+ * Status {200 : completed adding collection,
+ *        400 : user already exists}
+ *
+ */
+app.post("/create", async (req, res) => {
+  const username = req.body.username;
+  const test = await act.createCollection(username, {});
+  if (test.codeName == "NamespaceExists") {
+    res.status(400).send("username exists");
+  } else {
+    res.status(200).send("Good");
+  }
+});
+
+/**
+ * Creates documents for given user
+ * Used in pair with app.post('/create') to finish
+ * creating a users account with all 32 activities
+ *
+ * Headers : {username : name of the user}
+ * Status {200 : completed adding docs,
+ *        400 : Could not find the user}
+ *
+ */
+app.post("/createDoc", async (req, res) => {
+  const user = req.body.username;
+
+  const names = await act.listCollections().toArray();
+  let found = false;
+  for (let j = 0; j < names.length; j += 1) {
+    found = true;
+    if (names[j].name == user) {
+      for (let i = 1; i <= 32; i += 1) {
+        const addDoc = await act.collection(user).insertOne(activitySchema(i));
+      }
+    }
+  }
+
+  if (found) {
+    res.send(`Completed adding docs for user : ${user}`).status(200);
+  } else {
+    res.send(`Could not find user: ${user}`).status(400);
+  }
+});
+
 const start = async () => {
   try {
     app.listen(PORT, () => {
