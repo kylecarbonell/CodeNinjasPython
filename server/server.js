b1905a425199caf51523d0d6bf7a60d37420c1ae
Kyle Carbonell <kylecarbonell13@gmail.com>
2024-02-21 13:10:00 -0800

    Transitioning to server

--- server/server.js
+++ server/server.js
@@ -12,13 +12,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
