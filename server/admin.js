a043211fc53ee19c62217ff3e4fe3755411e03c1
Kyle Carbonell <kylecarbonell13@gmail.com>
2024-02-21 12:56:59 -0800

    Fixing cors issue

--- server/admin.js
+++ server/admin.js
@@ -12,7 +12,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-app.use(cors());
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
