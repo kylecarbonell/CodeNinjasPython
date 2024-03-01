799dfd429e3554f6db4beb6d89bbe899842d2f86
kylecarbonell <76983718+kylecarbonell@users.noreply.github.com>
2024-02-29 23:22:25 -0800

    TEST

--- python/app.py
+++ python/app.py
@@ -9,12 +9,53 @@ from io import StringIO
 import contextlib
 from contextlib import redirect_stdout
 
-from routes import main
 
 
-def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(main)
-    return app
+app = Flask(__name__)
 
+
+@app.route("/test", methods=['GET', 'POST'])
+def test():
+    data = json.loads(request.data)
+    print(data['code'])
+
+    loc = {}
+    response = jsonify({})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@contextlib.contextmanager
+def stdoutIO(stdout=None):
+    old = sys.stdout
+    if stdout is None:
+        stdout = StringIO()
+    sys.stdout = stdout
+    yield stdout
+    sys.stdout = old
+
+@app.route('/execute', methods=["GET", "POST"])
+def execute():
+    data = json.loads(request.data)
+    code = data['code']
+    # print(data)
+    # print(code)
+
+    response = {}
+    print("before")
+    print('in')
+    try:
+        f = StringIO()
+        with redirect_stdout(f):
+            # print("HELLO\n")
+            exec(code, {'a':  "poopoop"})
+            # print("\nDONE")
+            s = f.getvalue()
+            res = jsonify(s)
+            res.headers.add('Access-Control-Allow-Origin', '*')
+            return res
+    except Exception as e:
+        print("THIS IS E ", e)
+        res = jsonify("Error: " + str(e))
+        res.headers.add('Access-Control-Allow-Origin', '*')
+        return res
 
