769d49aff9a60d56191465a03f4d564dbf55c4d7
Kyle Carbonell <kylecarbonell13@gmail.com>
2024-03-14 23:27:34 -0700

    Working on grading now

--- server/Data/data.tsx
+++ server/Data/data.tsx
@@ -1,8 +1,8 @@
-const call = "http://localhost:8000"
-// const call = "https://codeninjaspython.onrender.com"
+// const call = "http://localhost:8000"
+const call = "https://codeninjaspython.onrender.com"
 
-const pythonCall = "http://127.0.0.1:5000";
-// const pythonCall = `https://codeninjaspythonserver.onrender.com`;
+// const pythonCall = "http://127.0.0.1:5000";
+const pythonCall = `https://codeninjaspythonserver.onrender.com`;
 
 
 export { call, pythonCall }
